ef43874c2559132056d0c5d69865fa89c32c91ab
TT <taozhang.tt@gmail.com>
2020-01-08 13:04:46 +0800

    etcd读写demo

---
+++ crontab/prepare/etcd_usage/demo2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main()  {
+	var (
+		client *clientv3.Client
+		err error
+		kv clientv3.KV
+		putResponse *clientv3.PutResponse
+	)
+	client, err = clientv3.New(clientv3.Config{
+		Endpoints: []string{"122.51.91.106:2379"},
+		DialTimeout: 5*time.Second,
+	})
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	//用于读写etcd 的键值对
+	kv = clientv3.NewKV(client)
+
+	//直接读写
+	//if putResponse, err = kv.Put(context.TODO(), "/cron/jobs/job1", "hello world"); err != nil{
+	//	fmt.Println(err)
+	//} else {
+	//	fmt.Println(putResponse.Header.Revision)
+	//}
+
+	//带 WithPrevKV 的读写
+	if putResponse, err = kv.Put(context.TODO(), "/cron/jobs/job1", "hello tt", clientv3.WithPrevKV()); err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("Revision: ", putResponse.Header.Revision)
+		if putResponse.PrevKv != nil {
+			fmt.Print("prevKv: ", string(putResponse.PrevKv.Value))
+		}
+	}
+}
