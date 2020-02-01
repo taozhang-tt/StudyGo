140b9a2e7a2c9c93d731899cee72d26d767c6bb9
TT <tt@TTs-MacBook-Pro.lan>
2020-02-01 11:23:01 +0800

    leetcode 52 bit解法

--- leet_code/0052-n-queens-ii/n-queens-ii.go
+++ leet_code/0052-n-queens-ii/n-queens-ii.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(totalNQueens(2))
+	fmt.Println(totalNQueens(0))
+	fmt.Println(totalNQueens2(0))
+
 }
 
 func totalNQueens(n int) int {
@@ -32,6 +34,32 @@ func dfs(n, row, total int, cols, pie, na map[int]bool) int {
 		delete(cols, j)
 		delete(pie, row+j)
 		delete(na, row-j)
+	}
+	return total
+}
+
+/**
+利用位运算（以4皇后为例说明）
+	每次按行遍历
+	col 记录列上的皇后位置，比如：0100，说明在第二列放置了一个皇后；
+	pie 记录皇后在撇上的攻击信息，比如：1000，说明当前行不能在第一列放置皇后；当向下扫描下一行时，应该将当前的 pie 左移一位
+	na  记录皇后在捺上的攻击信息，比如：0010，说明当前行不能在第三列放置皇后；当向下扫描下一行时，应该将当前的 na  右移一位
+
+ */
+func totalNQueens2(n int) int {
+	return dfsBit(n, 0, 0, 0, 0, 0)
+}
+
+func dfsBit(n, row, col, pie, na, total int) int {
+	if row >= n {
+		total ++
+		return total
+	}
+	bits := (^(col | pie | na)) & (1<<n-1)	//获取在该行上所有可以放置皇后的bit位（为1的位置）
+	for bits > 0 {
+		bit := bits & (-bits)	//获取可以放置皇后的最后一个bit位
+		total = dfsBit(n, row+1, col|bit, (pie|bit)<<1, (na|bit)>>1, total)
+		bits = bits & (bits-1)	//去除可以放置皇后的最后一个bit位
 	}
 	return total
 }
