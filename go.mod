2c7f57787ac2aa10bb41680430e3ff8754e468a3
TT <tt@TTs-MacBook-Pro.local>
2020-02-18 10:59:05 +0800

    leetcode 32

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module StudyGo
 
 go 1.13
 
-require github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75 // indirect
+require (
+	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75 // indirect
+	go.mongodb.org/mongo-driver v1.3.0 // indirect
+)
---
+++ leet_code/0032-longest-valid-parentheses/longest-valid-parentheses.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "()(()"
+	fmt.Println(longestValidParentheses2(s))
+	s = "(()"
+	fmt.Println(longestValidParentheses2(s))
+	s = ")()())"
+	fmt.Println(longestValidParentheses2(s))
+}
+
+/**
+动态规划解法：
+	状态定义：dp[i]，包含 a[i]在内，最长有效括号长度，当 a[j] 与 a[i] 匹配时（i>j）为了标记 a[j] 已被匹配，会赋值 dp[j] = dp[i]
+	状态转换：
+		（1）a[i] == '('
+			dp[i] = 0
+*/
+func longestValidParentheses(s string) int {
+	dp := make([]int, len(s))
+	max := 0
+	for i := 1; i < len(s); i++ {
+		if s[i] == ')' {
+			for j := i - 1; j >= 0; j-- {
+				if s[j] == '(' && dp[j] == 0 {
+					dp[j] = dp[i-1] + 2
+					if j >= 1 {
+						dp[j] += dp[j-1]
+					}
+					dp[i] = dp[j]
+					break
+				}
+			}
+			if dp[i] > max {
+				max = dp[i]
+			}
+		}
+	}
+	return max
+}
+
+func longestValidParentheses2(s string) int {
+	if len(s) <= 0 {
+		return 0
+	}
+	dp := make([]int, len(s))
+	sign := make([]int, len(s))
+	for i := 1; i < len(s); i++ {
+		dp[i] = dp[i-1]
+		if s[i] == ')' {
+			dp[i] = dp[i-1]
+			for j := i - 1; j >= 0; j-- {
+				if s[j] == '(' && sign[j] == 0 {
+					dp[i] += 2
+					sign[j] = 1
+					break
+				}
+			}
+		}
+	}
+	return dp[len(s)-1]
+}
