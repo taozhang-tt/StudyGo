b4bc3b9fd44dd8812e35539a886730b6ec641a8d
TT <tt@TTs-MacBook-Pro.local>
2020-04-03 11:30:42 +0800

    leetcode 7

---
+++ leet_code/0007-reverse-integer/reverse-integer.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+/**
+7. 整数反转
+	https://leetcode-cn.com/problems/reverse-integer/
+题目描述：
+	给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+示例 1:
+	输入: 123
+	输出: 321
+示例 2:
+	输入: -123
+	输出: -321
+示例 3:
+	输入: 120
+	输出: 21
+	注意:
+	假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+*/
+
+func main() {
+	var x = 123
+	fmt.Println(reverse(x))
+	x = -123
+	fmt.Println(reverse(x))
+	x = 120
+	fmt.Println(reverse(x))
+}
+
+func reverse(x int) int {
+	ret, sign, max, min := 0, 1, 2147483647, -2147483648
+	if x < 0 {
+		sign = -1
+		x = -x
+	}
+	for x > 0 {
+		ret = ret*10 + x%10
+		if ret*sign > max {
+			return 0
+		}
+		if ret*sign < min {
+			return 0
+		}
+		x /= 10
+	}
+	return ret * sign
+}
